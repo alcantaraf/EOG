2bb459998a63eb6fbda7478e4ef01e3ba78caf84
Davi Mendes <dmendes@aluno.unb.br>
2019-12-02 23:43:01 -0300

    Added Record Signals for some EOG Patterns

---
+++ EOG_signals/csv/processCsvRecordings.m
@@ -0,0 +1,21 @@
+clear all; close all; clc;
+
+% Author: Davi Mendes (@mendes-davi)
+% Goal: Process all RAW EOG records .csv files
+
+% Get dir info
+dir_info = dir();
+fnames = {dir_info.name};
+% Iterate for all files
+for f = 1:length(fnames)
+	if contains(fnames{f}, '.csv') % Select files with .csv extensions
+		data = load(fnames{f});
+		x = data(:,2);
+		time = data(:,1);
+		time = [time(1); time(:)];	
+		time = diff(time);
+		fs = ceil(1/mean(time));
+		t = linspace(0, length(x)/fs, length(x));
+		save([fnames{f}(1:end-4) '.mat'], 'x', 't', 'fs');
+	end	
+end
